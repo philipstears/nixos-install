6e2376149f8d9f96ed7a9c83d13b89deecea76ce
philipstears <philip@philipstears.com>
2019-11-06 11:34:29 +0000

    Live CD builder.

---
+++ scripts/live-cd/iso.nix
@@ -0,0 +1,19 @@
+{config, pkgs, ...}:
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+
+    # Provide an initial copy of the NixOS channel so that the user
+    # doesn't need to run "nix-channel --update" first.
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_5_1;
+
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    (import ../../common/stears/files/philip-pubkey.nix)
+  ];
+}
+
