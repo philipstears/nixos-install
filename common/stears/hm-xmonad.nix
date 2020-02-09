c57f8196157cf764268ea86d8942a028c0987a22
Philip Stears <philip@philipstears.com>
2020-02-09 16:56:47 +0000

    Lock screen shortcut no longer conflicts with XMonad's resize-main-column-right.

--- common/stears/hm-xmonad.nix
+++ common/stears/hm-xmonad.nix
@@ -45,25 +45,25 @@
               , layoutHook = noBorders $ layoutHook originalConfig
               }
               `additionalKeys`
-              [ ((0                         , xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 5%+")
-              , ((0                         , xF86XK_AudioLowerVolume), spawn "amixer -q set Master 5%-")
-              , ((0                         , xF86XK_AudioMute),        spawn "amixer -q set Master toggle")
-              , ((shiftMask                 , xF86XK_AudioRaiseVolume), spawn "amixer -q set Capture 5%+")
-              , ((shiftMask                 , xF86XK_AudioLowerVolume), spawn "amixer -q set Capture 5%-")
-              , ((shiftMask                 , xF86XK_AudioMute),        spawn "amixer -q set Capture toggle")
-              , ((0                         , xF86XK_AudioNext),        spawn "${pkgs.playerctl}/bin/playerctl next")
-              , ((0                         , xF86XK_AudioPrev),        spawn "${pkgs.playerctl}/bin/playerctl previous")
-              , ((0                         , xF86XK_AudioPlay),        spawn "${pkgs.playerctl}/bin/playerctl play-pause")
-              , ((0                         , xF86XK_AudioStop),        spawn "${pkgs.playerctl}/bin/playerctl stop")
+              [ ((0                                         , xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 5%+")
+              , ((0                                         , xF86XK_AudioLowerVolume), spawn "amixer -q set Master 5%-")
+              , ((0                                         , xF86XK_AudioMute),        spawn "amixer -q set Master toggle")
+              , ((shiftMask                                 , xF86XK_AudioRaiseVolume), spawn "amixer -q set Capture 5%+")
+              , ((shiftMask                                 , xF86XK_AudioLowerVolume), spawn "amixer -q set Capture 5%-")
+              , ((shiftMask                                 , xF86XK_AudioMute),        spawn "amixer -q set Capture toggle")
+              , ((0                                         , xF86XK_AudioNext),        spawn "${pkgs.playerctl}/bin/playerctl next")
+              , ((0                                         , xF86XK_AudioPrev),        spawn "${pkgs.playerctl}/bin/playerctl previous")
+              , ((0                                         , xF86XK_AudioPlay),        spawn "${pkgs.playerctl}/bin/playerctl play-pause")
+              , ((0                                         , xF86XK_AudioStop),        spawn "${pkgs.playerctl}/bin/playerctl stop")
 
-              , (((modMask originalConfig)  , xK_l),                    spawn "xdg-screensaver lock")
-              , (((modMask originalConfig)  , xK_o),                    spawn "otp")
-              , (((modMask originalConfig)  , xK_s),                    spawn "gnome-screenshot -i")
-              , (((modMask originalConfig)  , xK_c),                    spawn "mate-calc")
-              , (((modMask originalConfig)  , xK_n),                    spawn "networkmanager_dmenu")
+              , (((modMask originalConfig) .|. controlMask  , xK_l),                    spawn "xdg-screensaver lock")
+              , (((modMask originalConfig)                  , xK_o),                    spawn "otp")
+              , (((modMask originalConfig)                  , xK_s),                    spawn "gnome-screenshot -i")
+              , (((modMask originalConfig)                  , xK_c),                    spawn "mate-calc")
+              , (((modMask originalConfig)                  , xK_n),                    spawn "networkmanager_dmenu")
 
-              , (((modMask originalConfig)  , xK_e),                    spawn "st-kb-english")
-              , (((modMask originalConfig)  , xK_d),                    spawn "st-kb-german")
+              , (((modMask originalConfig)                  , xK_e),                    spawn "st-kb-english")
+              , (((modMask originalConfig)                  , xK_d),                    spawn "st-kb-german")
               ]
 
         toggleStrutsKey XConfig {XMonad.modMask = modMask} =
