f0d65e198eab42da13173cba37169b153efb0a6b
Philip Stears <philip@philipstears.com>
2020-03-16 19:09:58 +0000

    SIP firewall configuration.

--- hosts/sthades/configuration.nix
+++ hosts/sthades/configuration.nix
@@ -75,17 +75,17 @@
     # Allow the router to access services over the DMZ interface
     iptables -I nixos-fw 1 -i dmz -s 82.68.28.6 -p tcp -m tcp --dport 22 -j nixos-fw-accept
 
-    # Restricted access to SIP
-    iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p udp -m udp --dport 5060 -j nixos-fw-accept
-    iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p tcp -m tcp --dport 5060 -j nixos-fw-accept
-    iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p tcp -m tcp --dport 5061 -j nixos-fw-accept
-    iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p udp -m udp --dport 4000:4100 -j nixos-fw-accept
+    # # Restricted access to SIP
+    # iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p udp -m udp --dport 5060 -j nixos-fw-accept
+    # iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p tcp -m tcp --dport 5060 -j nixos-fw-accept
+    # iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p tcp -m tcp --dport 5061 -j nixos-fw-accept
+    # iptables -I nixos-fw 1 -i dmz -s 213.95.30.38 -p udp -m udp --dport 4000:4100 -j nixos-fw-accept
 
     # Allow anyone access to SIP (for temporary testing)
-    # iptables -I nixos-fw 1 -i dmz -p udp -m udp --dport 5060 -j nixos-fw-accept
-    # iptables -I nixos-fw 1 -i dmz -p tcp -m tcp --dport 5060 -j nixos-fw-accept
-    # iptables -I nixos-fw 1 -i dmz -p tcp -m tcp --dport 5061 -j nixos-fw-accept
-    # iptables -I nixos-fw 1 -i dmz -p udp -m udp --dport 4000:4100 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i dmz -p udp -m udp --dport 5060 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i dmz -p tcp -m tcp --dport 5060 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i dmz -p tcp -m tcp --dport 5061 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i dmz -p udp -m udp --dport 4000:4100 -j nixos-fw-accept
   '';
 
   networking.vlans = {
