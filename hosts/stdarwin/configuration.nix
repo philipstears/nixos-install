b2978ce0e3a8dec5ae48c19d08b85980cfaf2bd9
philipstears <philip@philipstears.com>
2020-04-01 23:39:45 +0100

    Preliminary config for stdarwin.

---
+++ hosts/stdarwin/configuration.nix
@@ -0,0 +1,219 @@
+# vim: set sts=2 ts=2 sw=2 expandtab :
+
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+      # Common things
+      ./../../common
+
+      # Hardware & user specific things
+      ./stears
+    ];
+
+  # Use the systemd-boot EFI boot loader - need
+  # this because we're doing whole-disk encryption.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = [
+    {
+      name = "cryptlvm";
+      device = "/dev/disk/by-label/system";
+      # preLVM = true;
+      allowDiscards = true;
+    }
+  ];
+
+  networking.hostName = "stdarwin";
+
+  # Name interfaces
+  services.udev.extraRules =
+    ''
+      KERNEL=="eth*", ATTR{address}=="54:b2:03:04:7d:68", NAME="trusted"
+      KERNEL=="eth*", ATTR{address}=="54:b2:03:04:7d:67", NAME="lab"
+    '';
+
+  # Open ports in the firewall.
+  networking.firewall.allowPing = true;
+
+  networking.firewall.interfaces.trusted = {
+    allowedTCPPorts = [
+      22    # SSH
+      5060  # SIP
+      3080  # Local HTTP
+      30443 # Local HTTPS
+    ];
+
+    allowedUDPPorts = [
+      5060  # SIP
+      5353  # mDNS
+    ];
+
+    allowedUDPPortRanges = [
+      { from = 4000; to = 4100; } # RTP
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    443 # NGINX (restrictions are handled by NGINX itself)
+    80  # NGINX (restrictions are handled by NGINX itself)
+  ];
+
+  networking.firewall.extraCommands = ''
+
+    # Allow the router to access services over the DMZ interface
+    iptables -I nixos-fw 1 -i dmz -s 82.68.28.6 -p tcp -m tcp --dport 22 -j nixos-fw-accept
+
+    # Allow anyone access to SIP (for temporary testing)
+    iptables -I nixos-fw 1 -i dmz -p udp -m udp --dport 5060 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i dmz -p tcp -m tcp --dport 5060 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i dmz -p tcp -m tcp --dport 5061 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i dmz -p udp -m udp --dport 4000:4100 -j nixos-fw-accept
+  '';
+
+  networking.vlans = {
+    dmz =   { id = 16; interface = "trusted"; };
+  };
+
+  networking.interfaces = {
+    dmz = {
+      ipv4 = {
+        addresses = [
+          { address = "82.68.28.2"; prefixLength = 29; }
+          { address = "82.68.28.1"; prefixLength = 29; }
+        ];
+      };
+    };
+  };
+
+  networking.defaultGateway = {
+    address = "82.68.28.5";
+    interface = "dmz";
+    metric = 10;
+  };
+
+  networking.nameservers = [];
+
+  networking.dhcpcd.extraConfig =
+    ''
+      # Disable DHCP on the DMZ
+      denyinterfaces dmz lab
+
+      # Disable APIPA addresses
+      noipv4ll
+
+      # Make sure the trusted LAN
+      # has a lower priority than
+      # than the DMZ
+      interface trusted
+      metric 100
+    '';
+
+  services.nginx = {
+    enable = true;
+
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    # Only allow PFS-enabled ciphers with AES256
+    sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+
+    commonHttpConfig = ''
+    '';
+
+    virtualHosts = {
+      "d2.philipstears.com" = {
+        forceSSL = true;
+        enableACME = true;
+
+        locations = {
+          "/" = {
+            proxyPass = "http://82.68.28.2:3080/";
+            proxyWebsockets = true;
+            extraConfig = ''
+              allow 82.68.28.0/29;
+              deny all;
+            '';
+          };
+        };
+      };
+
+      "d1.philipstears.com" = {
+        forceSSL = true;
+        enableACME = true;
+
+        locations = {
+          "/" = {
+            proxyPass = "http://82.68.28.1:3080/";
+            proxyWebsockets = true;
+            extraConfig = ''
+              allow 82.68.28.0/29;
+              deny all;
+            '';
+          };
+        };
+      };
+    };
+  };
+
+  security.acme.certs = {
+    "d2.philipstears.com".email = "philip@philipstears.com";
+    "d1.philipstears.com".email = "philip@philipstears.com";
+  };
+
+  # https://nixos.wiki/wiki/Accelerated_Video_Playback
+  hardware.opengl = {
+    enable = true;
+
+    # Let 32-bit application use OpenGL
+    driSupport32Bit = true;
+
+    extraPackages = with pkgs; [
+      vaapiVdpau
+    ];
+  };
+
+  # Use the proprietary nvidia driver
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # I'm using an Apple keyboard, make it sane
+  services.xserver.xkbOptions = "altwin:swap_alt_win,caps:ctrl_modifier";
+
+  virtualisation = {
+    virtualbox = {
+      host = {
+        enable = true;
+
+        # NOTE: This means that virtualbox needs compiling from source, which
+        # burns some CPU for a while
+        enableExtensionPack = true;
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+
+    # Vulkan!
+    vulkan-loader
+    vulkan-tools
+
+    # Wine with support for both 32-bit and 64-bit applications
+    (wineWowPackages.staging.override {
+      vulkanSupport = true;
+      # vkd3dSupport = true;
+    })
+
+    # Useful for QT things
+    hicolor-icon-theme
+  ];
+}
